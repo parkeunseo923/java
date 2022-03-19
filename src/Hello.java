5e90c10e197193658a5a0bba2a104a13bec41106
eunseo <parkeunseo923@gmail.com>
2022-03-19 21:38:45 +0900

    2주차 JAVA

--- src/Hello.java
+++ src/Hello.java
@@ -1,16 +1,11 @@
-/**
- * 첫 번째 예제 프로그램
- * @version 1.0 2022-03-12
- * @author 박은서
- */
-
-
 public class Hello {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello, Word!");    //Hello World! 출력 
-		System.out.println("I'm a new Java programer.");
+
+		String s1 = "Hello, Word!";    //Hello World! 출력 
+		String s2 = "I'm a new Java programer.";
+		
+		System.out.println (s1 + "\n" +s2);
 	}
 
 }
